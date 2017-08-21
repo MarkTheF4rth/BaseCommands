9728e03266b912240a175406d246b26f55fbb8d6
EC2 Default User <ec2-user@ip-172-31-19-4.eu-west-1.compute.internal>
2017-08-21 20:47:46 +0000

    improved help to accomodate new category help change

--- base_commands.py
+++ base_commands.py
@@ -3,37 +3,57 @@ from Decorators.command import command
 from Decorators.task import task
 from Decorators.func import func
 
+
+def construct_message(output_dict, lengths):
+    header = ['__**A list and brief description of each command you can use:**__'] 
+    message = header # for variable name clarity
+    command_length = max(lengths) 
+
+    if len(output_dict) == 1:
+        output = output_dict[list(output_dict)[0]] # don't use category headers if theres only 1 category
+        return header+['**`{:<{length}} :`** {}'.format(x, y, length=command_length) for x, y in output]
+
+    for category, commands in sorted(output_dict.items()): # sorts alphanumerically TODO: allow flexible sorting
+        message.append('\n**{} Commands**:'.format(category))
+        message.append('-'*20)
+        message = message + ['**`{:<{length}} :`** {}'.format(x, y, length=command_length) for x, y in commands]
+
+    return message
+
 @command(description='Displays this message')
 def help(self, message, ctx): 
     command_list = {} 
-    output = [] 
-    pm_output = [] 
-    header = ['__**A list and brief description of each command you can use:**__'] 
+    output = {} 
+    pm_output = {}
+    lengths = {'regular':[], 'pm':[]}
     msg_break = '**Continued...**' 
-    lengths = [] 
  
     for command_name, command in self.commands.items():
         description = command.get_description(message.channel, message.author.roles)
         if (not description) or (not command.validate_role(message.channel.id, message.author.roles)):
             continue
         command_name = '/'.join(command.aliases)
+        command_category = command.get_category(message.channel)
+
         if 'pm_help' in command.flags: 
-            pm_output.append((command_name, description))
+            output_dict = pm_output
+            lengths['pm'].append(len(command_name))
         else: 
-            output.append((command_name, description))
-        lengths.append(len(command_name)) 
- 
+            output_dict = output
+            lengths['regular'].append(len(command_name))
+
+        if command_category not in output_dict:
+            output_dict[command_category] = []
+        output_dict[command_category].append((command_name, description))
 
     if output: 
-        command_length = max(lengths) 
-        final_message_pb = header+['**`{:<{length}} :`** {}'.format(x, y, length=command_length) for x, y in output] 
+        final_message_pb = construct_message(output, lengths['regular'])
         self.message_printer('\n'.join(final_message_pb), message.channel, msg_break=msg_break) 
     else: 
         self.message_printer('No help message can be displayed at this time', message.channel)    
 
     if pm_output: 
-        command_length = max(lengths) 
-        final_message_pm = header+['**`{:<{length}} :`** {}'.format(x, y, length=command_length) for x, y in pm_output] 
+        final_message_pm = construct_message(pm_output, lengths['pm'])
         self.message_printer('\n'.join(final_message_pm), message.author, msg_break=msg_break) 
 
 @command(aliases=['c'], arglen=0, description='Confirms the bot is still alive')
