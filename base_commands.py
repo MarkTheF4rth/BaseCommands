a665137a9eefb53079f7346bd4560a1f917f25c2
EC2 Default User <ec2-user@ip-172-31-19-4.eu-west-1.compute.internal>
2017-10-17 23:08:49 +0000

    help now primarily sorts by the order of commands given in configs resolves issue #1

--- base_commands.py
+++ base_commands.py
@@ -3,17 +3,41 @@ from Decorators.command import command
 from Decorators.task import task
 from Decorators.func import func
 
+def sort_commands(commands, message, channel):
+    """Sorts given commands into those that need to be sent by PM and normally
+        also returns the longest command length in each case"""
 
-def construct_message(output_dict, lengths):
+    output = {'reg':{}, 'pm':{}}
+    lengths = {'reg':0, 'pm':0}
+    output_ref = {'True':'pm', False:'reg'} # maps result of command.pm_help to dict key
+
+    for command_name, command in commands.items():
+        description = command.get_description(message.channel, message.author.roles)
+        if (not description) or (not channel.validate_role(command_name, message.author.roles)):
+            continue
+        category_name = command.category_name
+        full_command_name = '/'.join(command.get_aliases())
+
+        output_type = output_ref[command.pm_help]
+        lengths[output_type] = max(lengths[output_type], len(full_command_name))
+
+        if category_name not in output[output_type]:
+            output[output_type][category_name] = {}
+        output[output_type][category_name].update({command_name :(full_command_name, description)})
+
+    return output, lengths
+
+
+def construct_message(output_dict, command_length):
+    """Create the mesage to be sent"""
     header = ['__**A list and brief description of each command you can use:**__'] 
     message = header # for variable name clarity
-    command_length = max(lengths) 
 
     if len(output_dict) == 1:
         output = output_dict[list(output_dict)[0]] # don't use category headers if theres only 1 category
         return header+['**`{:<{length}} :`** {}'.format(x, y, length=command_length) for x, y in output]
 
-    for category, commands in output_dict.items(): #sorted(output_dict.items()): # sorts alphanumerically TODO: allow flexible sorting
+    for category, commands in sorted(output_dict.items()): # sorts categories alphabetically 
         message.append('\n**{} Commands**:'.format(category))
         message.append('-'*20)
         message = message + ['**`{:<{length}} :`** {}'.format(x, y, length=command_length) for x, y in commands]
@@ -22,41 +46,48 @@ def construct_message(output_dict, lengths):
 
 @command(description='Displays this message')
 def help(self, message, ctx): 
-    command_list = {} 
-    output = {} 
-    pm_output = {}
-    lengths = {'regular':[], 'pm':[]}
+    """Displayed a formatted and sorted list of commands the user has access to"""
+    message_ref = {'reg':message.channel, 'pm':message.author} # connect message type to target channel
     msg_break = '**Continued...**' 
  
     channel = self.channels[message.channel.id]
-    for command_name, command in channel.commands.items():
-        description = command.get_description(message.channel, message.author.roles)
-        if (not description) or (not channel.validate_role(command_name, message.author.roles)):
+    output, lengths = sort_commands(channel.commands, message, channel)
+
+    for message_type, output_commands in output.items():
+        if not output_commands:
             continue
-        command_name = '/'.join(command.get_aliases())
-        category_name = command.category_name
 
-        if command.pm_help:
-            output_dict = pm_output
-            lengths['pm'].append(len(command_name))
-        else: 
-            output_dict = output
-            lengths['regular'].append(len(command_name))
+        formatted_commands = {}
 
-        if category_name not in output_dict:
-            output_dict[category_name] = []
-        output_dict[category_name].append((command_name, description))
+        for category, commands in output_commands.items(): # reformat and sort commands
+            new_commands = []
+            config_category = self.config['Categories'][category]
 
-    if output: 
-        final_message_pb = construct_message(output, lengths['regular'])
-        self.message_printer('\n'.join(final_message_pb), message.channel, msg_break=msg_break) 
-    else: 
-        self.message_printer('No help message can be displayed at this time', message.channel)    
+            if 'commands' in config_category:
+                config_commands = config_category['commands']
+            else:
+                config_commands = []
 
-    if pm_output: 
-        final_message_pm = construct_message(pm_output, lengths['pm'])
-        self.message_printer('\n'.join(final_message_pm), message.author, msg_break=msg_break) 
+            for command in config_commands:
+                if command in commands: # fail-safe, should normally be true
+                    new_commands.append(commands[command])
+                    del(commands[command])
+
+
+            formatted_commands[category] = new_commands + [
+                command for command_name, command in sorted(commands.items())]
+
+
+        final_message_pb = construct_message(formatted_commands, lengths[message_type])
+        self.message_printer('\n'.join(final_message_pb), message_ref[message_type],  msg_break=msg_break) 
+
+    if not output['reg'] and not output['pm']: # no help available
+        self.message_printer('No help message can be displayed at this time', message.channel)
+    elif not output['reg'] and output['pm']: # only PM available, inform channel
+        self.message_printer('A list of commands has been sent to you by PM', message.channel)
+
 
 @command(aliases=['c'], arglen=0, description='Confirms the bot is still alive')
 def confirm(self, message, ctx): 
+    """prints a message when called to confirm the bot can reach the specified channel"""
     self.message_printer('**I live**', message.channel)
