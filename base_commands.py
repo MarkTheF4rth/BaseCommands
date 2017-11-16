bbed05b3c3ac635349a5d8e6a0e11f881552ab10
EC2 Default User <ec2-user@ip-172-31-19-4.eu-west-1.compute.internal>
2017-11-16 11:13:30 +0000

    removed usage from confirm command

--- base_commands.py
+++ base_commands.py
@@ -3,27 +3,31 @@ from Decorators.command import command
 from Decorators.task import task
 from Decorators.func import func
 
-def sort_commands(commands, message, channel):
-    """Sorts given commands into those that need to be sent by PM and normally
-        also returns the longest command length in each case"""
+def sort_messages(commands, message, channel, command_char):
+    """Sorts messages into how they need to be sent, creates and returns the 
+        parts of the description to be formatted later. Keeps a track of the 
+        length of commands as it runs."""
 
     output = {'reg':{}, 'pm':{}}
     lengths = {'reg':0, 'pm':0}
     output_ref = {'True':'pm', False:'reg'} # maps result of command.pm_help to dict key
 
     for command_name, command in commands.items():
-        description = command.get_description(message.channel, message.author.roles)
-        if (not description) or (not channel.validate_role(command_name, message.author.roles)):
-            continue
-        category_name = command.category_name
+        if not channel.validate_role(command_name, message.author.roles):
+            continue 
+
         full_command_name = '/'.join(command.get_aliases())
+        command_usage = '{}{}{}'.format(command_char, command_name, (' '+command.get_usage()).rstrip())
+        description = command.get_description()
+
+        category_name = command.category_name
 
         output_type = output_ref[command.pm_help]
         lengths[output_type] = max(lengths[output_type], len(full_command_name))
 
         if category_name not in output[output_type]:
             output[output_type][category_name] = {}
-        output[output_type][category_name].update({command_name :(full_command_name, description)})
+        output[output_type][category_name].update({command_name :(full_command_name, command_usage, description)})
 
     return output, lengths
 
@@ -32,15 +36,17 @@ def construct_message(output_dict, command_length):
     """Create the mesage to be sent"""
     header = ['__**A list and brief description of each command you can use:**__'] 
     message = header # for variable name clarity
+    create_message = lambda output : (
+        ['**`{:<{length}} :`** usage = |**{}**| *{}*'.format(x, y, z, length=command_length) for x, y, z in output])
 
     if len(output_dict) == 1:
         output = output_dict[list(output_dict)[0]] # don't use category headers if theres only 1 category
-        return header+['**`{:<{length}} :`** {}'.format(x, y, length=command_length) for x, y in output]
+        return header+create_message(output)
 
     for category, commands in sorted(output_dict.items()): # sorts categories alphabetically 
         message.append('\n**{} Commands**:'.format(category))
         message.append('-'*20)
-        message = message + ['**`{:<{length}} :`** {}'.format(x, y, length=command_length) for x, y in commands]
+        message = create_message(output)
 
     return message
 
@@ -51,7 +57,7 @@ def help(self, message, ctx):
     msg_break = '**Continued...**' 
  
     channel = self.channels[message.channel.id]
-    output, lengths = sort_commands(channel.commands, message, channel)
+    output, lengths = sort_messages(channel.commands, message, channel, self.command_char)
 
     for message_type, output_commands in output.items():
         if not output_commands:
@@ -87,7 +93,7 @@ def help(self, message, ctx):
         self.message_printer('A list of commands has been sent to you by PM', message.channel)
 
 
-@command(aliases=['c'], arglen=0, description='Confirms the bot is still alive')
+@command(aliases=['c'], ('', '...')], description='Confirms the bot is still alive')
 def confirm(self, message, ctx): 
     """prints a message when called to confirm the bot can reach the specified channel"""
     self.message_printer('**I live**', message.channel)
