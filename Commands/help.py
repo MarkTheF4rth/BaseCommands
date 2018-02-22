4fdb2879e2400e88805825749294439f3195ef53
Ubuntu <ubuntu@ip-172-31-39-206.eu-west-1.compute.internal>
2018-02-22 18:35:32 +0000

    updated to conform to latest version of base bot
    pm_help is now a boolean

--- Commands/help.py
+++ Commands/help.py
@@ -4,17 +4,17 @@ from Decorators.task import task
 from Decorators.func import func
 
 def sort_messages(commands, message, channel, command_char):
-    """Sorts messages into how they need to be sent, creates and returns the 
-        parts of the description to be formatted later. Keeps a track of the 
+    """Sorts messages into how they need to be sent, creates and returns the
+        parts of the description to be formatted later. Keeps a track of the
         length of commands as it runs."""
 
     output = {'reg':{}, 'pm':{}}
     lengths = {'reg':0, 'pm':0}
-    output_ref = {'True':'pm', False:'reg'} # maps result of command.pm_help to dict key
+    output_ref = {True:'pm', False:'reg'} # maps result of command.pm_help to dict key
 
     for command_name, command in commands.items():
         if not channel.validate_role(command_name, message.author.roles):
-            continue 
+            continue
 
         full_command_name = '/'.join(command.get_aliases())
         command_usage = '{}{}{}'.format(command_char, command_name, (' '+command.get_usage()).rstrip())
@@ -34,7 +34,7 @@ def sort_messages(commands, message, channel, command_char):
 
 def construct_message(output_dict, command_length):
     """Create the mesage to be sent"""
-    header = ['__**A list and brief description of each command you can use:**__'] 
+    header = ['__**A list and brief description of each command you can use:**__']
     message = header # for variable name clarity
     create_message = lambda output : (
         ['**`{:<{length}} :`** usage = |**{}**| *{}*'.format(x, y, z, length=command_length) for x, y, z in output])
@@ -43,7 +43,7 @@ def construct_message(output_dict, command_length):
         output = output_dict[list(output_dict)[0]] # don't use category headers if theres only 1 category
         return header+create_message(output)
 
-    for category, commands in sorted(output_dict.items()): # sorts categories alphabetically 
+    for category, commands in sorted(output_dict.items()): # sorts categories alphabetically
         message.append('\n**{} Commands**:'.format(category))
         message.append('-'*20)
         message.extend(create_message(commands))
@@ -51,11 +51,11 @@ def construct_message(output_dict, command_length):
     return message
 
 @command(description='Displays this message')
-def help(self, message, ctx): 
+def help(self, message, ctx):
     """Displayed a formatted and sorted list of commands the user has access to"""
     message_ref = {'reg':message.channel, 'pm':message.author} # connect message type to target channel
-    msg_break = '**Continued...**' 
- 
+    msg_break = '**Continued...**'
+
     channel = self.channels[message.channel.id]
     output, lengths = sort_messages(channel.commands, message, channel, self.command_char)
 
@@ -85,7 +85,7 @@ def help(self, message, ctx):
 
 
         final_message_pb = construct_message(formatted_commands, lengths[message_type])
-        self.message_printer('\n'.join(final_message_pb), message_ref[message_type],  msg_break=msg_break) 
+        self.message_printer('\n'.join(final_message_pb), message_ref[message_type],  msg_break=msg_break)
 
     if not output['reg'] and not output['pm']: # no help available
         self.message_printer('No help message can be displayed at this time', message.channel)
